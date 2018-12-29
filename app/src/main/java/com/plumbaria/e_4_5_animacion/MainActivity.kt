33747ba4e8fa5d3752e67dea91550307055a4f09
Quico Orozco <esi@plumbaria.com>
2018-12-29 20:38:21 +0100

    Terminado Ejercicio 4.5. AnimationDrawable

--- app/src/main/java/com/plumbaria/e_4_5_animacion/MainActivity.kt
+++ app/src/main/java/com/plumbaria/e_4_5_animacion/MainActivity.kt
@@ -1,12 +1,28 @@
 package com.plumbaria.e_4_5_animacion
 
-import android.support.v7.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var animacion:AnimationDrawable
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        animacion = ContextCompat.getDrawable(this.applicationContext,R.drawable.animacion) as AnimationDrawable
+        var imageView:ImageView = ImageView(this)
+        imageView.setBackgroundColor(Color.WHITE)
+        imageView.setImageDrawable(animacion)
+        imageView.setOnClickListener(this)
+
+        setContentView(imageView)
+    }
+
+    override fun onClick(v: View?) {
+        animacion.start()
     }
 }
